dd5d3a66a694195c906a706ca2e7c057e755c8de
Nivetha32 <nivethaparu325r@gmail.com>
2024-05-15 03:47:17 +0000

    Sync LeetCode submission Runtime - 4120 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ 0183-customers-who-never-order/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+Select Name as Customers
+
+from Customers
+
+where id != All(select c.id
+
+                from Customers c, Orders o
+
+                where c.id = o.Customerid)
