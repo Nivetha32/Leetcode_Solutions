9d0ee265dc50e3aa0a0030007c17a6691fec9ebd
Nivetha32 <nivethaparu325r@gmail.com>
2024-09-16 14:32:01 +0000

    Sync LeetCode submission Runtime - 936 ms (5.09%), Memory - 20.9 MB (93.23%)

--- 0217-contains-duplicate/solution.py
+++ 0217-contains-duplicate/solution.py
@@ -2,8 +2,6 @@ class Solution(object):
     def containsDuplicate(self, nums):
         nums.sort()
         for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+            if nums[i] == nums[i-1]:
                 return True
         return False
-
-        
