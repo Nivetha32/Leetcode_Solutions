30b89ca9def2b4ba033f2676eab2c230d251c2f8
Nivetha32 <nivethaparu325r@gmail.com>
2025-09-26 15:22:37 +0000

    Sync LeetCode submission Runtime - 22 ms (99.76%), Memory - 43.5 MB (100.00%)

---
+++ 0611-valid-triangle-number/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int c=0;
+        for(int k =n-1;k>=2;k--)
+        {
+            int i=0,j=k-1;
+            while(i<j)
+            {
+                if(nums[i]+nums[j]>nums[k])
+                {
+                    c+=(j-i);
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        return c;
+    }
+}
