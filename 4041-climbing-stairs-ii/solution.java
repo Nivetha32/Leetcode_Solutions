8ec2b411a9afbac18bde79e8298ef8e00818e572
Nivetha32 <nivethaparu325r@gmail.com>
2025-09-27 15:38:30 +0000

    Sync LeetCode submission Runtime - 7 ms (70.97%), Memory - 63.9 MB (100.00%)

---
+++ 4041-climbing-stairs-ii/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n, int[] costs) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            if (i - 1 >= 0) dp[i] = Math.min(dp[i], dp[i - 1] + costs[i - 1] + 1*1);
+            if (i - 2 >= 0) dp[i] = Math.min(dp[i], dp[i - 2] + costs[i - 1] + 2*2);
+            if (i - 3 >= 0) dp[i] = Math.min(dp[i], dp[i - 3] + costs[i - 1] + 3*3);
+        }
+
+        return dp[n];
+    }
+}
+
