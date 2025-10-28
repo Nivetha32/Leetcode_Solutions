700e711b7505a06981f2d485c52da71c0903d45f
Nivetha32 <nivethaparu325r@gmail.com>
2025-10-28 16:04:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.4 MB (100.00%)

---
+++ 1610-xor-operation-in-an-array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int nums[] = new int[n];
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=start+2*i;
+            k=k^nums[i];
+        }
+        return k;
+    }
+}
