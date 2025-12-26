178f60d82bf8a05163b6621b42439adf965d2b60
Nivetha32 <nivethaparu325r@gmail.com>
2025-12-26 09:25:55 +0000

    Sync LeetCode submission Runtime - 7 ms (51.99%), Memory - 20.9 MB (100.00%)

--- 0347-top-k-frequent-elements/solution.py
+++ 0347-top-k-frequent-elements/solution.py
@@ -1,15 +1,7 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-      heap = []
-      for x,y in Counter(nums).items():
-        heap.append((-y,x))
-      heapq.heapify(heap)
-      re = []
-      for i in range(min(k,len(heap))):
-        re.append(heapq.heappop(heap)[1])
-      return re
-
-
       
-        
+      freq = Counter(nums)
+      return sorted(freq, key = freq.get,reverse =True)[:k]
+      
