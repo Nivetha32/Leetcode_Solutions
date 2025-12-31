ab72de37a880eebfbcc72f4a4590d17c1bd00ecb
Nivetha32 <nivethaparu325r@gmail.com>
2025-12-31 08:53:12 +0000

    Sync LeetCode submission Runtime - 341 ms (100.00%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
+select id from (select id, recordDate,temperature,LAG(temperature) OVER (ORDER BY recordDate) AS p_temp ,   LAG(recordDate) OVER (ORDER BY recordDate) AS p_date from Weather) t
+where temperature > p_temp
+and DATEDIFF(recordDate, p_date) = 1;
 
 
-select a.Id
 
-from weather a, weather b
 
-where a.Temperature>b.Temperature and datediff(a.recorddate,b.recorddate)=1
+
