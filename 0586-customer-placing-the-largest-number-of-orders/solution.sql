352f1138ffe5541c5b833edb9dae4f2d4c24ac5b
Nivetha32 <nivethaparu325r@gmail.com>
2025-12-31 04:18:54 +0000

    Sync LeetCode submission Runtime - 402 ms (100.00%), Memory - 0.0B (100.00%)

--- 0586-customer-placing-the-largest-number-of-orders/solution.sql
+++ 0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -1,20 +1,6 @@
 # Write your MySQL query statement below
-With t1 as 
-
-(
-
-  Select customer_number, 
-
-  Rank() over(order by count(customer_number) desc) as rk
-
-  from orders
-
-  group by customer_number
-
-) 
-
-Select t1.customer_number
-
-from t1
-
-where t1.rk=1
+select customer_number 
+from Orders
+group by customer_number
+order by count(*) desc
+limit 1
