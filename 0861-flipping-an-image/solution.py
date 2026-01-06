6f4d06886580387976f64ec1e12a75c3f56759b3
Nivetha32 <nivethaparu325r@gmail.com>
2026-01-06 06:27:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (22.48%)

--- 0861-flipping-an-image/solution.py
+++ 0861-flipping-an-image/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        m = []
-        for i in image:
-            h=[]
-            for k in i[::-1]:
-                if k==0:
-                    h.append(1)
-                else:
-                    h.append(0)
-            m.append(h)
-        return m
+        for r in image:
+            r.reverse()
+            for i in range(len(r)):
+                r[i]=1-r[i]
+        return image
+
