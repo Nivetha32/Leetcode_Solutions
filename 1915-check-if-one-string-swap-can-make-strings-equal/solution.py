4fca54a7da826d303dfbfb838cb88182a3b04593
Nivetha32 <nivethaparu325r@gmail.com>
2026-01-21 07:10:35 +0000

    Sync LeetCode submission Runtime - 1 ms (13.93%), Memory - 19.3 MB (64.50%)

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        dif=[]
+        for i in range(len(s1)):
+            if s1[i]!=s2[i]:
+                dif.append(i)
+        if len(dif)==0:
+            return True
+        if len(dif)==2:
+            i,j = dif
+            if s1[i] == s2[j] and s1[j] == s2[i]:
+                return True
+        return False
