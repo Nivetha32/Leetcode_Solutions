4c9dc5eb7b0ce8301eba0fdc638d79149c972882
Nivetha32 <nivethaparu325r@gmail.com>
2026-01-22 14:12:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (8.35%)

---
+++ 1283-reformat-date/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        date,month,year = date.split()
+        months = {
+          "Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06",
+           "Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"
+        }
+        date = date[:-2].zfill(2)
+        month_y = months[month]
+        k = "-".join([year,month_y,date])
+        return k
+
+    
+        
