0e6723ad74eb86f71a342d58cb6278dc68972858
Nivetha32 <nivethaparu325r@gmail.com>
2026-01-27 16:49:13 +0000

    Sync LeetCode submission Runtime - 3 ms (90.03%), Memory - 20.4 MB (46.68%)

---
+++ 0724-find-pivot-index/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        t = sum(nums)
+        l=0
+
+        for  i in range(len(nums)):
+            if l==t-l-nums[i]:
+                return i
+            l+=nums[i]
+        return -1
