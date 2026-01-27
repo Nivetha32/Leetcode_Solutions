be94dfe80938c94aac491a1b09769021fd12fb94
Nivetha32 <nivethaparu325r@gmail.com>
2026-01-27 15:50:18 +0000

    Sync LeetCode submission Runtime - 42 ms (72.94%), Memory - 19.3 MB (24.70%)

---
+++ 0278-first-bad-version/solution.py
@@ -0,0 +1,13 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l , r = 1,n
+        while l<r :
+            m = (r+l)//2
+            if not isBadVersion(m):
+                l = m+1
+            else:
+                r=m
+        return l
