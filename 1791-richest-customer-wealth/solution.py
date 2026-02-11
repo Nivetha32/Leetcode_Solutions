e6c472ddc787447c47ae22e540aac2ab33589168
Nivetha32 <nivethaparu325r@gmail.com>
2026-02-11 04:59:38 +0000

    Sync LeetCode submission Runtime - 1 ms (7.99%), Memory - 19.3 MB (41.35%)

--- 1791-richest-customer-wealth/solution.py
+++ 1791-richest-customer-wealth/solution.py
@@ -1,5 +1,6 @@
-class Solution(object):
-    def maximumWealth(self, accounts):
-      return max(sum(acc) for acc in accounts)
-
-        
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        m=0
+        for i in accounts:
+           m = max(m,sum(i))
+        return m
