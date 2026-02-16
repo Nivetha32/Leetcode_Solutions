73d59e234b8410ef73a2d8d3662778bd3e679da7
Nivetha32 <nivethaparu325r@gmail.com>
2026-02-16 04:51:33 +0000

    Sync LeetCode submission Runtime - 175 ms (81.61%), Memory - 41.6 MB (22.41%)

--- 4217-first-element-with-unique-frequency/solution.py
+++ 4217-first-element-with-unique-frequency/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def firstUniqueFreq(self, nums: List[int]) -> int:
         m = Counter(nums)
-        u = Counter(m.values())
+        n = Counter(m.values())
         for i in nums:
-            if u[m[i]]==1:
+            if n[m[i]]==1:
                 return i
         return -1
+        
