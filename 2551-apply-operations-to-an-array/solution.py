336d968c001e5be50687d714ae4497083e198ca5
Nivetha32 <nivethaparu325r@gmail.com>
2026-02-23 16:09:31 +0000

    Sync LeetCode submission Runtime - 3 ms (37.93%), Memory - 19.5 MB (22.83%)

---
+++ 2551-apply-operations-to-an-array/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+               nums[i]=nums[i]*2
+               nums[i+1]=0
+        p=[]
+        c=0
+        for i in nums:
+            if i==0:
+                c+=1
+            else:
+                p.append(i)
+        k = [0]*c
+        return p+k
